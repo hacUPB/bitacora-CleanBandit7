6e3444954486fcc8bcae2fc3881e1e1ef2954ac3
Juan David Ibañez <ibanezherrerajuandavid17@gmail.com>
2025-07-17 15:28:17 -0500

    Se guardaron correctamente los archivos de la unidad 1 en la carpeta de investigación

---
+++ Actividad_1/Investigación/Clase1_SC.asm
@@ -0,0 +1,15 @@
+@16384
+D=A
+@16
+M=D
+//Seguimos con (Queremos sumarle 20 al valor del registro 16)
+@20
+D=A
+@16
+M=D+M
+
+//(Ahora escribe un programa en lenguaje ensamblador que guarde en la posición 32 de la RAM un 100.)
+@100
+D=A
+@32
+M=D
